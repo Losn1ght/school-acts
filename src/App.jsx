65b420e3c5e59e712417f0ee76ed82c5e3d31cfc
Jedd Christian Juab <132525756+Losn1ght@users.noreply.github.com>
2023-09-27 10:00:08 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,24 @@ import About from './pages/about'
 import './app.css'
 
 function App() {
+  const [title, setTitle] = useState("")
+
+  function getData(){
+    fetch(`https://dog.ceo/api/breeds/image/random`)
+    .then(response=> response.json())
+    .then(json=> {
+      console.log(json)
+      setTitle(json.message)
+    })
+  }
+
 
   return (
     <>
+      <div className="image">
+          <img src={title}></img>
+      </div>
+      <button onClick={getData} >Click Me</button>
       <BrowserRouter>
       <header>
         <nav>
